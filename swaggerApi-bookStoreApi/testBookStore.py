678fb9977ab8f32c4a8f83564d87270d2742a195
tamar samara <108719616+TamarSamara@users.noreply.github.com>
2022-08-27 00:25:25 +0300

    Update testBookStore.py

--- swaggerApi-bookStoreApi/testBookStore.py
+++ swaggerApi-bookStoreApi/testBookStore.py
@@ -2,11 +2,8 @@ import logging
 import pytest
 from api.book_store_api import BookstoreApi
 import requests
-import json
 from data import *
 from models.book_store import BookStore
-from testAccount import start as startAccount
-
 
 
 @pytest.fixture(scope="session")
@@ -18,14 +15,16 @@ def start(pytestconfig):
     api = BookstoreApi(url, header)
     return api
 
+
 @pytest.mark.passed
-def test_get_bookstore(start):
+def test_get_books(start):
     """
     Check if "get_bookstore" has returned all the books in the store
     passed 200
     """
     logging.info('Check if "get_books" has returned all the books in the store')
     res = start.get_bookstore()
+    assert res.status_code == 200
     books = res.json()
     book_list = []
     for i in books['books']:
@@ -33,61 +32,75 @@ def test_get_bookstore(start):
         book_list.append(b.to_json())
     for i in range(len(book_list)):
         # to check only title
-        logging.info(f'Successfully get book')
+        logging.info(f'Successfully get books, status code is: {res.status_code}')
         logging.warning(f'Error: {res.reason}, status code is: {res.status_code}')
         assert book_list[i].split(":")[2].split(",")[0][2:-1] == BOOK_TITLE_LIST[i]
 
-    assert res.status_code == 200
 
-
-@pytest.mark.failed
-def test_put_bookstore_invalid_isbn(start):
+@pytest.mark.passed
+def test_get_book_by_userID(start):
     """
-    Check "put_bookstore" with invalid ISBN
-    failed 400
+    Check if you get a book by user id
+    passed 200
     """
-    logging.info('Check "put_bookstore" with invalid ISBN')
-    res = start.put_bookstore_by_isbn("97849325862", ISBN)
-    logging.info(f'Successfully put book')
+    logging.info('Check if you get a book by user id')
+    res = start.get_book_by_ID(ISBN)
+    logging.info(f'Successfully get book by user id, status code is: {res.status_code}')
     logging.warning(f'Error: {res.reason}, status code is: {res.status_code}')
     assert res.status_code == 200
-
-@pytest.mark.failed
-def test_put_bookstore_empty_isbn(start):
-    """
-    Check "put_bookstore" with invalid ISBN
-    failed 400
-    """
-    logging.info('Check "put_bookstore" with invalid ISBN')
-    res = start.put_bookstore_by_isbn("9781449325862", {})
-    logging.info(f'Successfully put book')
-    logging.warning(f'Error: {res.reason}, status code is: {res.status_code}')
-    assert res.status_code == 200
+    assert res.json()["isbn"] == ISBN
 
 
 @pytest.mark.passed
-def test_get_bookSore():
+def test_add_list_books(start):
     """
-    Check if you get books from the bookstore
-    passed
+    Check if add list of books
+    passed 200
     """
-    logging.info("get book store")
-    getBookStore = BookstoreApi().get_bookstore()
-    print(f' staaaaaaaaattuuusss code {getBookStore.status_code}')
-    logging.info("get bookstore")
-    assert getBookStore.status_code == 200
+    logging.info('Check if add list of books')
+    res = start.post_add_list_of_books(ListOfBooks)
+    logging.info(f'Successfully add list of books, status code is: {res.status_code}')
+    logging.warning(f'Error: {res.reason}, status code is: {res.status_code}')
+    assert res.status_code == 200
+    assert res.reason == "OK"
+
 
 @pytest.mark.passed
-def test_post_bookstore():
+def test_delete_books_to_user(start):
     """
-    Check to add an existing user
-    passed
+    Verify deleting books by user ID
+    passed 200
     """
-    logging.info("add a new user to the store")
+    logging.info('deleting books by user ID')
+    res = start.delete_books(userID)
+    logging.info(f'Books have been successfully deleted by user ID, status code is: {res.status_code}')
+    logging.warning(f'Error: {res.reason}, status code is: {res.status_code}')
+    assert res.status_code == 200
+    assert res.reason == "OK"
+    assert userID not in res.text
 
-    postbook = BookApi().post_bookStore(data)
-    logging.info("Successful Post Book")
-    logging.warning(f'Error, status code is {postbook.status_code}')
-    assert postbook.status_code == 200
 
-#
+@pytest.mark.passed
+def test_delete_book_by_string_object(start):
+    """
+    Verify deleting book by string Object
+    passed 200 -> 401 not Unauthorized :(
+    """
+    logging.info('deleting book by string Object ')
+    res = start.delete_book(DELACCOUNT)
+    logging.info(f'Books have been successfully deleted by string Object, status code is: {res.status_code}')
+    logging.warning(f'Error: {res.reason}, status code is: {res.status_code}')
+    assert res.status_code == 200
+    assert res.reason == "OK"
+
+
+def test_put_book(start):
+    """
+    Check if the book is "put" by isbn
+    """
+    logging.info('Check if the book is "put" by isbn')
+    res = start.put_bookstore_by_isbn(ISBN2, PUT_USER)
+    logging.info(f'Books have been successfully putted by isbn, status code is: {res.status_code}')
+    logging.warning(f'Error: {res.reason}, status code is: {res.status_code}')
+    assert res.status_code == 200
+    assert res.reason == "OK"

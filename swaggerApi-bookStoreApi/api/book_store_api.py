1a1514b94376a3834b042e43929d76fb4cc94584
tamar samara <108719616+TamarSamara@users.noreply.github.com>
2022-08-27 00:29:44 +0300

    Update book_store_api.py

--- swaggerApi-bookStoreApi/api/book_store_api.py
+++ swaggerApi-bookStoreApi/api/book_store_api.py
@@ -1,7 +1,5 @@
 import requests
 from api.baseApi import BaseApi
-from models.account import Account
-from models.book_store import BookStore
 
 
 class BookstoreApi(BaseApi):
@@ -9,23 +7,26 @@ class BookstoreApi(BaseApi):
         super().__init__(url, header)
         self._url = f"{self._base_url}BookStore/v1/"
 
-
     def get_bookstore(self) -> [list]:
         res = self.session.get(url=f"{self._url}Books")
         return res
 
-    def post_bookstore(self, books: dict) -> requests.Response:
-        res = self.session.post(url=f"{self._url}Books", data=books)
+    def post_add_list_of_books(self, books: dict) -> requests.Response:
+        res = self.session.post(url=f"{self._url}s", data=books, headers=self._headers)
         return res
 
-    def delete_bookstore(self, user_id: str) -> requests.Response:
-        res = self.session.delete(url=f"{self._url}Books/?UserId={user_id}")
-        return res
-
-    def put_bookstore_by_isbn(self, isbn: str, data: dict):
-        res = self.session.put(url=f"{self._url}Books/{isbn}", data=data)
+    def delete_books(self, user_id: str) -> requests.Response:
+        res = self.session.delete(url=f"{self._url}", data=user_id, headers=self._headers)
         return res
 
     def delete_book(self, data: dict) -> requests.Response:
         res = self.session.delete(url=f"{self._url}Book", data=data)
+        return res
+
+    def put_bookstore_by_isbn(self, isbn: str, data: dict):
+        res = self.session.put(url=f"{self._url}s/{isbn}", data=data, headers=self._headers)
+        return res
+
+    def get_book_by_ID(self, isbn: str):
+        res = self.session.get(url=f"{self._url}Book?ISBN={isbn}")
         return res
